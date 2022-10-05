bec7b1caf557c7e09b5208bfc753fdea6a856ef5
cfayehc <37842767+cfayehc@users.noreply.github.com>
2022-10-05 10:26:46 -0400

    Working Error Rate By Position.

--- ErrorRateByPosition.R
+++ ErrorRateByPosition.R
@@ -1,133 +1,69 @@
+#same as ErrorRateByPosition2 on 10/5/2022 - Works 
+
 library(Lahman)
 library(dplyr)
 
 #set the working directory
 setwd("E:/Research/Lahman-master")
 
+getResidue <- function(x,y,svar) {
+  #  x<-d$Chance
+  #  y<-d$svar
+  relation<-lm(y~x)
+  relation<-lm(formula = y~x)
+  print(relation)
+  summary(relation)
+  
+  graphlabel<-paste(svar," vs Chances all players",ppos,">",GG,"Games",sep=" ")
+  #graphname<-paste("EvA",ppos,"linear.png",sep="-")
+  #png(file=graphname)
+  plot(x=x,y=y, xlab="Chances", ylab=svar, main=graphlabel,col="blue",abline(lm(y~x)),cex=1.3,pch=16)
+  #dev.off()
+  svar.res=resid(relation)
+  #  d<-cbind(d,svar.res)
+  #  print(d)
+}
+
 #Populate rangef with rf =range factor, rfg = rangefactor per game, rfga=adjust rangefactor per game, chance, 
 #fieldp = fielding percentage, E.rate=rate of errors per chance
 rangef <- select(Fielding,playerID, yearID, POS, G, InnOuts, PO, A, E)
-rangef <- mutate(rangef,playerID, yearID, POS, G, PO, A, E, rf=(PO+A), rfg=rf/G, rfga=(PO+A-E)/G, Chance=PO+A+E, fieldp=rf/Chance, E.rate=E/Chance,)
+rangef <- mutate(rangef,playerID, yearID, POS, G, PO, A, E, rf=(PO+A), rfg=rf/G, Chance=PO+A+E, fieldp=rf/Chance)
+#,  E.rate=E/Chance,)]
+#dont need E.rate as E.rate = 1 - fieldingp
 
 #Choose Position and make a variable based on the position select number of games
 #filter list by position and number of games played  --and year?
 GG=82
-ppos="3B"  
+ppos="2B"  
 year=1910
+year2=2022
 
 str1<-"rf"
 dfname<-paste(str1,ppos,sep="")
 #filter the rangefactor by position and number of games
 #assign(dfname,filter(rangef,G>GG,POS==ppos))
-assign(dfname,filter(rangef,G>GG,POS==ppos,yearID>=year))
+assign(dfname,filter(rangef,G>GG,POS==ppos,yearID>=year & yearID<=year2))
 #print(get(dfname))
 
-######################################################################
-#Plot E/chance and fit
-x<-get(dfname)$Chance
-y<-get(dfname)$E
-graphlabel<-paste("Errors vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#plot(x=x,y=y, xlab="Chances", ylab="Errors", main=graphlabel)
-Erelation<-lm(y~x)
-Erelation<-lm(formula = y~x)
-print(Erelation)
-summary(Erelation)
+residues<-get(dfname)
 
-graphlabel<-paste("Errors vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#graphname<-paste("EvA",ppos,"linear.png",sep="-")
-#png(file=graphname)
-plot(x=x,y=y, xlab="Chances", ylab="Errors", main=graphlabel,col="blue",abline(lm(y~x)),cex=1.3,pch=16)
-#dev.off()
+E.res<-getResidue(get(dfname)$Chance,get(dfname)$E,'E')
+residues<-cbind(residues,E.res)
 
-######################################################################
-#Plot rf/chance and fit
-#x<-get(dfname)$Chance
-y<-get(dfname)$rf
-#graphlabel<-paste("rf vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#plot(x=x,y=y, xlab="Chances", ylab="range factor", main=graphlabel)
-rfrelation<-lm(y~x)
-rfrelation<-lm(formula = y~x)
-print(rfrelation)
-summary(rfrelation)
+#rf.res<-getResidue(get(dfname)$Chance,get(dfname)$rf,'rf') #same as -E.res
+#residues<-cbind(residues,rf.res)
 
-graphlabel<-paste("range factor vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#graphname<-paste("EvA",ppos,"linear.png",sep="-")
-#png(file=graphname)
-plot(x=x,y=y, xlab="Chances", ylab="range factor", main=graphlabel,col="blue",abline(lm(y~x)),cex=1.3,pch=16)
-#dev.off()
+PO.res<-getResidue(get(dfname)$Chance,get(dfname)$PO,'PO')
+residues<-cbind(residues,PO.res)
 
-######################################################################
-#Plot PO/chance and fit
-#x<-get(dfname)$Chance
-y<-get(dfname)$PO
-#graphlabel<-paste("rf vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#plot(x=x,y=y, xlab="Chances", ylab="range factor", main=graphlabel)
-POrelation<-lm(y~x)
-POrelation<-lm(formula = y~x)
-print(POrelation)
-summary(POrelation)
+A.res<-getResidue(get(dfname)$Chance,get(dfname)$A,'A')
+residues<-cbind(residues,A.res)
 
-graphlabel<-paste("Putouts vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#graphname<-paste("POvA",ppos,"linear.png",sep="-")
-#png(file=graphname)
-plot(x=x,y=y, xlab="Chances", ylab="Putouts", main=graphlabel,col="blue",abline(lm(y~x)),cex=1.3,pch=16)
-#dev.off()
+residues<-cbind(residues,pE=residues$E.res/get(dfname)$Chance)  #probability of E.res per chance?
+assign(dfname,residues)
 
-######################################################################
-#Plot Assists/chance and fit
-#x<-get(dfname)$Chance
-y<-get(dfname)$A
-#graphlabel<-paste("Assists vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#plot(x=x,y=y, xlab="Chances", ylab="Assists", main=graphlabel)
-Arelation<-lm(y~x)
-Arelation<-lm(formula = y~x)
-print(Arelation)
-summary(Arelation)
+topp <- filter(get(dfname), (rank(E.res)<=20))
+print(topp[order(-topp$rf), ] )
 
-graphlabel<-paste("Assists vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#graphname<-paste("AvCh",ppos,"linear.png",sep="-")
-#png(file=graphname)
-plot(x=x,y=y, xlab="Chances", ylab="Assists", main=graphlabel,col="blue",abline(lm(y~x)),cex=1.3,pch=16)
-#dev.off()
-
-######################################################################
-#A check Plot (PO+Assists)/chance and fit
-#x<-get(dfname)$Chance
-#y<-get(dfname)$A+get(dfname)$PO
-#graphlabel<-paste("range factor vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#plot(x=x,y=y, xlab="Chances", ylab="range factor", main=graphlabel)
-#rf2relation<-lm(y~x)
-#rf2relation<-lm(formula = y~x)
-#print(rf2relation)
-#summary(rf2relation)
-
-#graphlabel<-paste("Range factor vs Chances all players",ppos,">",GG,"Games",sep=" ")
-#graphname<-paste("rf2vCh",ppos,"linear.png",sep="-")
-#png(file=graphname)
-#plot(x=x,y=y, xlab="Chances", ylab="range factor", main=graphlabel,col="blue",abline(lm(y~x)),cex=1.3,pch=16)
-#dev.off()
-#looks like range factor woo!
-
-######################################################################
-#calculate residues
-#dfnameres <- paste(get(dfname),"res",sep=".")
-error.res=resid(Erelation) #error residue == difference between linear model and number of errors
-rf.res=resid(rfrelation) #error residue == difference between linear model and number of errors
-PO.res=resid(POrelation) #error residue == difference between linear model and number of errors
-A.res=resid(Arelation) #error residue == difference between linear model and number of errors
-
-
-######################################################################
-residues<-cbind(get(dfname),E.res=error.res, rf.res, PO.res, A.res)
-print(residues[order(residues$E.res), ] )
-pE<-c(residues$E.res/residues$Chance) #What percent of chances is the error residue
-residues<-cbind(residues,pE)
-print(residues[order(residues$pE), ] )
-
-people <- select(tbl_df(People), playerID, nameLast, nameFirst)#add names
-residues <- merge(residues, people, all.x = TRUE)
-print(residues[order(residues$pE), ] )
-
-topp <- filter(residues, (rank(pE)<=100))
-print(topp[order(topp$pE), ] )
-
+#rf.res2<-getResidue(get(dfname)$rfg,get(dfname)$E.res,'rf.res')
+#residues<-cbind(get(dfname),rf.res2)
